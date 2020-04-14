ff8bfe031c73641feb79704d8a011154e3c1c873
Pirmin Kager <pirmink@gmail.com>
2020-04-14 10:00:38 +0200

    new

--- cnc.py
+++ cnc.py
@@ -8,26 +8,28 @@ import os
 import argparse
 
 ## Variable declaration
+defaultport = '/dev/ttys000'
+buffersize = 10
 
 serialsettings = {'parity': 'N',
-		  'baudrate': 32400,
-		  'bytesize': 8,
-		  'xonxoff': False,
-		  'rtscts': False,
-		  'timeout': 10,
-		  'inter_byte_timeout': None,
-		  'stopbits': 1,
-		  'dsrdtr': False,
-		  'write_timeout': None}
+                  'baudrate': 32400,
+                  'bytesize': 8,
+                  'xonxoff': False,
+                  'rtscts': False,
+                  'timeout': 10,
+                  'inter_byte_timeout': None,
+                  'stopbits': 1,
+                  'dsrdtr': False,
+                  'write_timeout': None}
 
 ## Function Definitions
 
 def openSerial(port,arguments):
     try:
         ser = serial.Serial(port)
-        ser.applySettingsDict(arguments)
-        ser.flushInput()
-        ser.flushOutput()
+        ## ser.applySettingsDict(arguments)
+        ## ser.flushInput()
+        ## ser.flushOutput()
     except:
         print("Couldn't open %s" % (port))
         end()
@@ -36,31 +38,26 @@ def openSerial(port,arguments):
 def openFile(file):
     print file
     if file != '':
-	try:
-	    f = open(file, "r")
-	except:
-	    print("Couldn't open %s" % (file))
-	    end()
+        try:
+            f = open(file, "r")
+        except:
+            print("Couldn't open %s" % (file))
+            end()
     else:
-	## Get Filename from input
+    ## Get Filename from input
         file = raw_input('Filename or Path: ')
-	f = openFile(file)
+        f = openFile(file)
     return f
 
 def parseArgs():
     ##Definition
     parser = argparse.ArgumentParser(description='VHFterm')
-    parser.add_argument('File', metavar='P', type=str, nargs='?', default='',
-                        help='Path to the HPGL file')
-    parser.add_argument('Device', metavar='d', type=str, nargs='?', default='/dev/pty/0',
-		        help='Path to the device')
+    parser.add_argument('File', metavar='P', type=str, nargs='?', default='',help='Path to the HPGL file')
+    parser.add_argument('Device', metavar='d', type=str, nargs='?', default=defaultport,help='Path to the device')
     args = parser.parse_args()
     return args
 
 args = parseArgs()
-
-def exit():
-    raise SystemExit
 
 def splitFile(f):
     list = f.read()
@@ -70,26 +67,44 @@ def splitFile(f):
         list[i] = list[i] + ";"
     return list
 
-def sendFile(f):
-    pass
 
-def 
 
 ## AS MODULE ##
 if __name__ != '__main__':
-
     def end():
-	pass
-
+        pass
     args = parseArgs()
-
 
 ## SCRIPTMODE ##
 if __name__ == '__main__':
-
     def end():
-	close()
-  ## Parse Arguments
+        ser.close()
+        quit()
+    ## Parse Arguments
     args = parseArgs()
-    openFile(args.File)
-    openSerial(args.Device,serialsettings)
+    f = openFile(args.File)
+    list = splitFile(f)
+    ser = openSerial(args.Device,serialsettings)
+    eof = False
+    index = 0
+    buffer = 0
+    lenlist = len(list)
+    while eof != True:
+        try:
+            if buffer < buffersize:
+                ser.write(list[index])
+                index = index + 1
+                buffer = buffer + 1
+            bytesavailable = ser.inWaiting()
+            if bytesavailable > 0:
+                reponse = ser.Read(bytesavailable)
+                print(response)
+                buffer = buffer - response.count(';')
+
+            if index == lenlist:
+                eof = True
+
+        except:
+            print("Error while sending file")
+            end()
+    print
